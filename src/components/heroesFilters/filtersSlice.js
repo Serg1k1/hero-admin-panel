0a219188e98ad6b11cc00e83110e434c58798a55
Serhii <kindratsergij60@gmail.com>
2024-02-19 16:55:23 +0200

    feat: done an admin panel

---
+++ src/components/heroesFilters/filtersSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
+import { useHttp } from '../../hooks/http.hook';
+
+// const initialState = {
+//     filters: [],
+//     filtersLoadingStatus: 'idle',
+//     activeFilter: 'all'
+// }
+
+const filtersAdapter = createEntityAdapter();
+
+const initialState = filtersAdapter.getInitialState({
+    filtersLoadingStatus: 'idle',
+    activeFilter: 'all'
+})
+
+export const filterHeroes = createAsyncThunk(
+    "filters/filterHeroes",
+    async () => {
+        const { request } = useHttp();
+        return await request("http://localhost:3001/filters");
+    }
+)
+
+const filtersSlice = createSlice({
+    name: 'filters',
+    initialState,
+    reducers: {
+        filtersActiveFilterChanged: (state, action) => {
+            state.activeFilter = action.payload;
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(filterHeroes.pending, state => { state.filtersLoadingStatus = 'loading' })
+            .addCase(filterHeroes.fulfilled, (state, action) => {
+                state.filtersLoadingStatus = 'idle';
+                filtersAdapter.setAll(state, action.payload);
+            })
+            .addCase(filterHeroes.rejected, state => {
+                state.filtersLoadingStatus = 'error';
+            })
+            .addDefaultCase(() => { })
+    }
+});
+
+
+export const { selectAll } = filtersAdapter.getSelectors((state) => state.filters);
+
+const { actions, reducer } = filtersSlice;
+export default reducer;
+export const {
+    filtersActiveFilterChanged,
+} = actions;
---
+++ src/components/heroesListItem/HeroesListItem.js
@@ -0,0 +1,96 @@
+const HeroesListItem = ({ name, description, element, onDelete }) => {
+
+    let elementClassName;
+
+    switch (element) {
+        case 'fire':
+            elementClassName = 'bg-danger bg-gradient';
+            break;
+        case 'water':
+            elementClassName = 'bg-primary bg-gradient';
+            break;
+        case 'wind':
+            elementClassName = 'bg-success bg-gradient';
+            break;
+        case 'earth':
+            elementClassName = 'bg-secondary bg-gradient';
+            break;
+        default:
+            elementClassName = 'bg-warning bg-gradient';
+    }
+
+    return (
+        <li
+            className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
+            <img src="https://cdn4.iconfinder.com/data/icons/political-elections/50/48-512.png"
+                className="img-fluid w-25 d-inline"
+                alt="unknown hero"
+                style={{ 'objectFit': 'cover' }} />
+            <div className="card-body">
+
+                <h3 className="card-title">{name}</h3>
+                <p className="card-text">{description}</p>
+            </div>
+            <span onClick={onDelete}
+                className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
+                <button type="button" className="btn-close btn-close" aria-label="Close"></button>
+            </span>
+        </li>
+    )
+}
+
+export default HeroesListItem;
+
+
+
+// import { heroDeleted } from '../../actions';
+// import { useDispatch } from 'react-redux';
+// import { useHttp } from '../../hooks/http.hook';
+
+// const HeroesListItem = ({ name, description, element, id }) => {
+//     const dispatch = useDispatch();
+//     const { request } = useHttp();
+//     let elementClassName;
+
+//     switch (element) {
+//         case 'fire':
+//             elementClassName = 'bg-danger bg-gradient';
+//             break;
+//         case 'water':
+//             elementClassName = 'bg-primary bg-gradient';
+//             break;
+//         case 'wind':
+//             elementClassName = 'bg-success bg-gradient';
+//             break;
+//         case 'earth':
+//             elementClassName = 'bg-secondary bg-gradient';
+//             break;
+//         default:
+//             elementClassName = 'bg-warning bg-gradient';
+//     }
+
+//     const deleteItem = (id) => {
+//         dispatch(heroDeleted(id));
+//         request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+//     }
+
+//     return (
+//         <li
+//             className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
+//             <img src="https://cdn4.iconfinder.com/data/icons/political-elections/50/48-512.png"
+//                 className="img-fluid w-25 d-inline"
+//                 alt="unknown hero"
+//                 style={{ 'objectFit': 'cover' }} />
+//             <div className="card-body">
+
+//                 <h3 className="card-title">{name}</h3>
+//                 <p className="card-text">{description}</p>
+//             </div>
+//             <span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
+//                 <button onClick={() => deleteItem(id)} type="button" className="btn-close btn-close" aria-label="Close"></button>
+//             </span>
+//         </li>
+//     )
+// }
+
+// export default HeroesListItem;
